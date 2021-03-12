ac59f13fad7a465f06c7e6dd8ea62fb1382675dc
gpatruno <56818714+gpatruno@users.noreply.github.com>
2021-03-12 15:27:20 +0100

    Add Natives Components and Splitters librairie

--- app/src/main/java/com/student/basic_ui/SecondFragment.kt
+++ app/src/main/java/com/student/basic_ui/SecondFragment.kt
@@ -1,31 +1,68 @@
 package com.student.basic_ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
+import com.student.basic_ui.databinding.FragmentSecondBinding
+import splitties.toast.longToast
+import splitties.toast.toast
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
 class SecondFragment : Fragment() {
 
+    private var _binding: FragmentSecondBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
+            inflater: LayoutInflater,
+            container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        Log.i(SecondFragment::class.simpleName, "onCreateView")
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        super.onViewCreated(binding.root, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        Log.i(SecondFragment::class.simpleName, "onViewCreated")
+
+        //binding.fab.setOnClickListener { toast(R.string.text_island) }
+
+        binding.fab.setOnClickListener { longToast(R.string.text_island) }
+
+        //binding.btnAlert.setOnClickListener { showAlertDialog() }
+
+        //binding.btnAlert.setOnClickListener { share(R.string.text_share, R.string.title_share) }
+
+        binding.buttonSecond.setOnClickListener { findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment) }
+
+        binding.btnSnackBar.setOnClickListener { view ->
+            Snackbar.make(view, R.string.text_label, Snackbar.LENGTH_SHORT)
+                .setAction("Action") {
+                    toast(R.string.text_island)
+            }.show()
         }
+    }
+
+    /*private fun showAlertDialog() {
+        alertDialog {
+            messageResource = R.string.text_alert
+            okButton { showAlertDialog() }
+            cancelButton()
+        }.onShow {
+            positiveButton.setText(R.string.action_like)
+        }.show()
+    }*/
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
